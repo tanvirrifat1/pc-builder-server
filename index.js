0efcfc8f253b8e1bf519db6c8e76c451daf0692d
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-07-30 15:00:34 +0600

    added more updated

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.afkplob.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, {
@@ -20,6 +21,7 @@ const run = async () => {
   try {
     const db = client.db("pc-builder");
     const productCollection = db.collection("allPc");
+    const userCollection = db.collection("user");
 
     app.get("/allPc", async (req, res) => {
       const cursor = productCollection.find({});
@@ -39,8 +41,37 @@ const run = async () => {
     app.get("/allPc/:category", async (req, res) => {
       const category = req.params.category;
       const result = await productCollection.find({ category }).toArray();
-      console.log(result);
       res.send({ status: true, data: result });
+    });
+
+    app.post("/user", async (req, res) => {
+      try {
+        const user = req.body;
+        const result = await userCollection.insertOne(user);
+        console.log(result);
+        res
+          .status(201)
+          .json({ message: "User created successfully", user: result.ops[0] });
+      } catch (error) {
+        console.error("Error:", error);
+        res
+          .status(500)
+          .json({ error: "An error occurred while creating the user" });
+      }
+    });
+
+    app.get("/user", async (req, res) => {
+      try {
+        const cursor = await userCollection.find({});
+        const product = await cursor.toArray();
+        console.log(cursor);
+        res.send({ status: true, data: product });
+      } catch (error) {
+        console.error("Error:", error);
+        res
+          .status(500)
+          .json({ error: "An error occurred while fetching users" });
+      }
     });
   } finally {
   }
